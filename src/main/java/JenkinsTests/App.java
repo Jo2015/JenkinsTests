3e53b1a5aa34392d7faefef4e7cc9b308dc3bb2c
Jamal Oufkir <jamal.oufkir.1@ens.etsmtl.ca>
2022-06-28 19:37:10 -0400

    initial commit

---
+++ src/main/java/JenkinsTests/App.java
@@ -0,0 +1,13 @@
+package JenkinsTests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
